e17f2903a093db1ed2817dab89d878ae47f647d0
Will Studabaker <will.studabaker@gmail.com>
2020-10-17 18:35:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -27,4 +27,12 @@ function doWhileLoop(integer){
     console.log('I run once regardless.')
   }while(incrementVariable() < integer)
 }*/
-function forLoop(){}
+function forLoop(arr){
+  for (let i = 0; i< 25; i++){
+    if(i === 1){
+      arr.push(`"I am 1 strange loop."`)
+    } else {
+      arr.push(`"I am ${i} strange loops."`)
+    }
+  }
+}
